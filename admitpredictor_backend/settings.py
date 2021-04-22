adddfa527e96cc9729a62f8e34bde962901c360a
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-04-23 02:27:00 +0530

    configured using sqllite

--- admitpredictor_backend/settings.py
+++ admitpredictor_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zfpk6xknccg@(&@$r3p!l&!%_lh&(x=)www3ei$kj!39ceuumm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bhqqqb8355.execute-api.ap-south-1.amazonaws.com']
+ALLOWED_HOSTS = ['127.0.0.1','bhqqqb8355.execute-api.ap-south-1.amazonaws.com']
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'admitpredictor_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
