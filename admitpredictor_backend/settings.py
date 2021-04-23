00c8935adb7a11ede4bccd7c8eba0003f4e661de
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-04-23 23:56:39 +0530

    added postgres creation to build workflow

--- admitpredictor_backend/settings.py
+++ admitpredictor_backend/settings.py
@@ -76,40 +76,15 @@ WSGI_APPLICATION = 'admitpredictor_backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'USER': 'default',
-    #     'NAME': 'default',
-    #     'PASSWORD': '',
-    #     'HOST' : 'localhost',
-    #     'PORT' : '5432'
-    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'USER': '',
-        'NAME': '',
-        'PASSWORD': '',
-        'HOST' : '',
-        'PORT' : ''
-    },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-}
-
-if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
+        'NAME': 'github_actions',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
