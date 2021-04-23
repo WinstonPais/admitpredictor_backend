0a330a7b45a16546afaeeba27ddf54e4c3622476
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-04-23 15:14:50 +0530

    Update settings.py

--- admitpredictor_backend/settings.py
+++ admitpredictor_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'admitpredictor_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'mydatabaseuser',
-        'NAME': 'mydatabase',
-        'TEST': {
-            'NAME': 'mytestdatabase',
-        },
+        'USER': 'mupplito',
+        'NAME': 'mupplito',
+        'PASSWORD': 'oeD5fK7l_gwaX0mIJu5_HPJqWXBeGx0x',
+        'HOST' : 'queenie.db.elephantsql.com',
+        'PORT' : '5432'
     },
 }
 
