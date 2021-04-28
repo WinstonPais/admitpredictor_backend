9c87dcbffca144f40084dfb91c98c75a09c3af42
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-04-29 04:17:19 +0530

    additional test for getmodel with wrong input

--- mainapp/tests/tests_helperFunctions.py
+++ mainapp/tests/tests_helperFunctions.py
@@ -11,3 +11,4 @@ class TestGetModelFunction(unittest.TestCase):
         ML_MODELS_FOLDER = Path(__file__).resolve().parent.parent.parent.joinpath('Ml_models')
         model = joblib.load(ML_MODELS_FOLDER.joinpath('sklearn_LinearRegression').joinpath('sklearn_LinearRegression.pkl'))
         self.assertEqual(type(getModel('sklearn_LinearRegression')), type(model))
+        self.assertEqual(getModel('rubbish'), None)
