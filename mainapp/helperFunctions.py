4789f26f10682beace6847ebc9dff9cca9ca8a78
Wilton <wiltondsouza100@gmail.com>
2021-06-26 21:49:06 +0530

    Update helperFunctions.py
    Ran tests for tensor_flow model

--- mainapp/helperFunctions.py
+++ mainapp/helperFunctions.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import load_model
 ML_MODELS_FOLDER = Path(__file__).resolve().parent.parent.joinpath('Ml_models')
 
 def getModel(algorithm):
+    model = None
     try:
         if algorithm == "sklearn_LinearRegression":
             model = joblib.load(ML_MODELS_FOLDER.joinpath(f'{algorithm}').joinpath(f'{algorithm}.pkl'))
