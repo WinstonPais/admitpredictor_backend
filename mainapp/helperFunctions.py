af239cf1cb151b2c61a8a6dc9a44c2eaa46cbf10
Wilton <wiltondsouza100@gmail.com>
2021-06-26 21:36:58 +0530

    tensorflow updated
    updated with url.py and views.py

--- mainapp/helperFunctions.py
+++ mainapp/helperFunctions.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 import joblib
+from tensorflow.keras.models import load_model
 
 ML_MODELS_FOLDER = Path(__file__).resolve().parent.parent.joinpath('Ml_models')
 
 def getModel(algorithm):
     try:
-        model = joblib.load(ML_MODELS_FOLDER.joinpath(f'{algorithm}').joinpath(f'{algorithm}.pkl'))
+        if algorithm == "sklearn_LinearRegression":
+            model = joblib.load(ML_MODELS_FOLDER.joinpath(f'{algorithm}').joinpath(f'{algorithm}.pkl'))
+        elif algorithm == "tensorflow_neuralNetworks":
+            model = load_model(ML_MODELS_FOLDER.joinpath(f'{algorithm}').joinpath(f'{algorithm}.h5'))
     except:
         return None
 
